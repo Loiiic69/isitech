f08643c7a6694ab43d9055824d761aae2f1f8c3e
Loiiic69 <105201726+Loiiic69@users.noreply.github.com>
2022-12-22 11:23:05 +0100

    Add files via upload

--- jeu.js
+++ jeu.js
@@ -1,54 +1,58 @@
+const { userInfo } = require("os");
+const { exit } = require("process");
 const readline = require("readline");
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
-
-// Ask the user for a choice rock, paper, or scissor.
-var playerChoice = "";
-var counter = 0;
-
-// playerChoice = prompt("Make a selection: rock, paper, or scissors: ");
-
-    rl.question("Enter rock, paper, or scissors: ",function(res){
-        playerChoice = res
-        playGame()
-        counter++;
-    });
-
-// Pick rack, paper, or scissors for the computer.
-function playGame(){
+// Demande a l'utilisateur son choix
+function jeu() {
+  console.clear();
+  var playerChoice = "";
+  var counter = 0;
+  rl.question("Choissez Pierre, Papier ou Ciseaux ", function (res) {
+    playerChoice = res
+    playGame()
+    counter++;
+    rl.close
+  });
+// Choix aléatoire pour l'ordinateur.
+  function playGame() {
     var computerChoice = "";
-
-var randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-if (randomNumber == 1){
-  computerChoice = "rock"
-}else if (randomNumber == 2){
-  computerChoice = "paper"
-} else if (randomNumber == 3){
-  computerChoice = "scissors"
-} else {
-  console.log("Math is broken");
-  exit;
+    var randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    if (randomNumber == 1) {
+      computerChoice = "Pierre"
+    } else if (randomNumber == 2) {
+      computerChoice = "Papier"
+    } else if (randomNumber == 3) {
+      computerChoice = "Ciseaux"
+    } else {
+      console.log("Math is broken");
+      exit;
+    }
+  // Resultat
+    console.log("Votre choix: ", playerChoice);
+    console.log("Choix de votre adversaire: ", computerChoice);
+    if (playerChoice === computerChoice) {
+      console.log("Match nul !");
+    } else if (playerChoice === 'Papier' && computerChoice === 'Pierre') {
+      console.log('Vous avez gagné!');
+      console.log(playerChoice, "Gagne contre", computerChoice);
+    } else if (playerChoice === 'Ciseaux' && computerChoice === 'Papier') {
+      console.log('Vous avez gagné!');
+      console.log(playerChoice, "Gagne contre", computerChoice);
+    } else if (playerChoice === 'Pierre' && computerChoice === 'Ciseaux') {
+      console.log('Vous avez gagné!');
+      console.log(playerChoice, "Gagne contre", computerChoice);
+    } else {
+      console.log('Vous avez perdu!');
+      console.log(computerChoice, "Gagne contre", playerChoice);
+    }
+  ///Fin du jeu et rédémarrage du jeu
+    rl.question("Voulez vous quitter le programme (O / N)?", function (res) {
+      if (res === "o") process.exit(0);
+      else if (res === "n") jeu();
+    })
+  }
 }
-
-console.log("Player chose: ", playerChoice);
-console.log("Computer Chose: ", computerChoice);
-
-if (playerChoice === computerChoice){
-  console.log("Tie game!");
-} else if (playerChoice === 'paper' && computerChoice === 'rock'){
-  console.log('Player Wins!');
-  console.log(playerChoice ,"beats", computerChoice);
-} else if (playerChoice === 'scissors' && computerChoice === 'paper'){
-  console.log('Player Wins!');
-  console.log(playerChoice ,"beats", computerChoice);
-} else if (playerChoice === 'rock' && computerChoice === 'scissors'){
-  console.log('Player Wins!');
-  console.log(playerChoice ,"beats", computerChoice);
-} else {
-  console.log('Computer Wins!');
-  console.log(computerChoice, "beats", playerChoice);
-}
-}
+jeu();
